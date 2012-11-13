f6d194fc726677a6a7f265910a38e88aeb664495
nbari <nbari@inbox.im>
2012-11-13 10:35:57 +0000

    Improvement on the regenerate_id method, adding the client IPv4 of the client to the fingerprint, helping to prevent an HTTP session hijacking attacks. 	modified:   classes/DALMP/Sessions.php

--- classes/DALMP/Sessions.php
+++ classes/DALMP/Sessions.php
@@ -345,8 +345,8 @@ class DALMP_Sessions {
       if ($num_blocks > 4) {
         $num_blocks = 4;
       }
-      if (isset($_SERVER['REMOTE_ADDR'])) { // pending validation for ipv6
-        $blocks = explode('.', $_SERVER['REMOTE_ADDR']);
+      if ($ip = $this->getIPv4()) { // pending validation for ipv6
+        $blocks = explode('.', $ip);
         for ($i = 0; $i < $num_blocks; $i++) {
           $fingerprint.= $blocks[$i] . '.';
         }
@@ -364,6 +364,25 @@ class DALMP_Sessions {
     } else {
       return false;
     }
+  }
+
+	/**
+   * getIPv4 - find the IP address of the client
+   *
+   * @return IPv4
+   */
+  public function getIPv4() {
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+      if (array_key_exists($key, $_SERVER) === true){
+        foreach (explode(',', $_SERVER[$key]) as $ip){
+          $ip = trim($ip);
+          if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false) {
+            return $ip;
+          }
+        }
+      }
+    }
+    return isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : false;
   }
 
 }
