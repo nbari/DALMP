ed96c123f0aa3c756c4d35577d6ed3706e7acbf6
nbari <nbari@dalmp.com>
2013-06-12 23:08:18 +0100

    new file:   DALMP/Cache.php 	new file:   DALMP/Cache/Cache.php 	new file:   DALMP/Dalmp.php 	new file:   DALMP/Database.php 	new file:   DALMP/Loader.php 	new file:   DALMP/Logger.php 	new file:   DALMP/Sessions.php 	new file:   dalmp.php

---
+++ lib/DALMP/Loader.php
@@ -0,0 +1,23 @@
+<?php
+namespace DALMP;
+
+/**
+ * Loader - Autoloader
+ *
+ * @author Nicolas de Bari Embriz <nbari@dalmp.com>
+ * @package DALMP
+ * @license BSD License
+ * @version 3.0
+ */
+class Loader {
+
+  public static function autoload($className) {
+    $className = ltrim($className, '\\');
+    require DALMP_DIR . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+  }
+
+  public static function register() {
+    spl_autoload_register(array('DALMP\Loader', 'autoload'));
+  }
+
+}
---
+++ lib/dalmp.php
@@ -0,0 +1,21 @@
+<?php
+namespace DALMP;
+
+/**
+ * DALMP - Database Abstraction Layer for MySQL using PHP
+ *
+ * @author Nicolas de Bari Embriz <nbari@dalmp.com>
+ * @package DALMP
+ * @license BSD License
+ * @version 3.0
+ */
+
+if (defined('DALMP_DIR')) {
+  return;
+} else {
+  define('DALMP_DIR', dirname(__FILE__));
+}
+
+require_once DALMP_DIR . '/DALMP/Loader.php';
+
+Loader::register();
