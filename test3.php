68e6f6c041589d886ffa77f5ade61346ea77e786
nbari <nbari@dalmp.com>
2013-07-02 14:53:44 +0100

    modified:   lib/DALMP/Sessions.php   new file:   lib/DALMP/Sessions/SQLite.php   modified:   lib/DALMP/Sessions/base.php   renamed:    lib/DALMP/Sessions.php -> lib/DALMP/Sessions/old.php   new file:   test3.php

---
+++ test3.php
@@ -0,0 +1,14 @@
+<?php
+// Measure Page Load Time
+require_once 'mplt.php';
+$timer = new mplt();
+/**
+ * require the DALMP class
+ */
+require_once 'lib/dalmp.php';
+
+$ses = new DALMP\Sessions();
+
+
+# -----------------------------------------------------------------------------------------------------------------
+echo PHP_EOL,str_repeat('-', 80),PHP_EOL,'Time: ',$timer->getPageLoadTime(),' - Memory: ',$timer->getMemoryUsage(1),PHP_EOL,str_repeat('-', 80),PHP_EOL;
