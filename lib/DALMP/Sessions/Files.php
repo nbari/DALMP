d15317037b9c1695662692b8cef37a4f14528247
nbari <nbari@dalmp.com>
2013-07-24 11:26:52 +0100

    modified:   lib/DALMP/Sessions/Files.php 	modified:   lib/DALMP/Sessions/SQLite.php

--- lib/DALMP/Sessions/Files.php
+++ lib/DALMP/Sessions/Files.php
@@ -46,21 +46,25 @@ class Files implements \SessionHandlerInterface {
   }
 
   public function destroy($session_id) {
-    $sess_path = $this->savePath . '/' . substr($session_id, 0, 2) . '/' . substr($session_id, 2, 2) . '/' . substr($session_id, 4, 2) . '/';
-    $sess_file = $sess_path . "{$session_id}.sess";
+    $sess_path = sprintf('%s/%s/%s/%s', $this->savePath, substr($session_id, 0, 2), substr($session_id, 2, 2),  substr($session_id, 4, 2));
+    $sess_file = sprintf('%s/%s', $sess_path , "{$session_id}.sess");
+
     if (file_exists($sess_file)) {
       unlink($sess_file);
     }
+
     return True;
   }
 
   public function gc($maxlifetime) {
     $session_files = $this->rsearch($this->savePath, '#^.*\.sess$#');
+
     foreach ($session_files as $file) {
       if (filemtime($file) + $maxlifetime < time() && file_exists($file)) {
         unlink($file);
       }
     }
+
     return True;
   }
 
@@ -69,28 +73,34 @@ class Files implements \SessionHandlerInterface {
   }
 
   public function read($session_id) {
-    $sess_path = $this->savePath . '/' . substr($session_id, 0, 2) . '/' . substr($session_id, 2, 2) . '/' . substr($session_id, 4, 2) . '/';
+    $sess_path = sprintf('%s/%s/%s/%s', $this->savePath, substr($session_id, 0, 2), substr($session_id, 2, 2),  substr($session_id, 4, 2));
+
     if (!is_dir($sess_path) && !mkdir($sess_path, 0700, True)) {
       throw new \Exception("$sess_path  not accessible");
     }
-    $sess_file = $sess_path . "{$session_id}.sess";
+
+    $sess_file = sprintf('%s/%s', $sess_path , "{$session_id}.sess");
+
     return (string) @file_get_contents($sess_file);
   }
 
   public function write($session_id, $session_data) {
-    $sess_path = $this->savePath . '/' . substr($session_id, 0, 2) . '/' . substr($session_id, 2, 2) . '/' . substr($session_id, 4, 2) . '/';
+    $sess_path = sprintf('%s/%s/%s/%s', $this->savePath, substr($session_id, 0, 2), substr($session_id, 2, 2),  substr($session_id, 4, 2));
+
     if (!is_dir($sess_path) && !mkdir($sess_path, 0700, True)) {
       throw new \Exception("$sess_path  not accessible");
     }
-    $sess_file = $sess_path . "{$session_id}.sess";
+
+    $sess_file = sprintf('%s/%s', $sess_path , "{$session_id}.sess");
+
     return file_put_contents($sess_file, $session_data) === False ? False : True;
   }
 
   /**
-   * getSessionsRefs - get all sessions containing references
+   * getSessionsRefs
    *
    * @param int $expiry
-   * @return array of sessions
+   * @return array of sessions containing any reference
    */
   public function getSessionsRefs($expired_sessions = False) {
     $refs = array();
@@ -98,17 +108,17 @@ class Files implements \SessionHandlerInterface {
   }
 
   /**
-   * getSessionsRef - get session containing a specific reference
+   * getSessionRef
    *
    * @param string $ref
-   * @return array sessions
+   * @return array of session containing a specific reference
    */
   public function getSessionRef($ref) {
     return False;
   }
 
   /**
-   * del sessions ref - delete sessions containing a specific reference
+   * delSessionsRef - delete sessions containing a specific reference
    *
    * @param string $ref
    * @return boolean
@@ -128,7 +138,7 @@ class Files implements \SessionHandlerInterface {
     $ite = new RecursiveIteratorIterator($dir);
     $files = new RegexIterator($ite, $pattern, RegexIterator::GET_MATCH);
     $fileList = array();
-    foreach($files as $file) {
+    foreach ($files as $file) {
       $fileList = array_merge($fileList, $file);
     }
     return $fileList;
