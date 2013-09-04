203f2377825d4f4c1063781e0983af22fd5f3810
nbari <nbari@dalmp.com>
2013-09-04 17:15:42 +0100

    fixed uuid regex to be uuid 4 compliant 	modified:   tests/test_dalmp.php

--- tests/test_dalmp.php
+++ tests/test_dalmp.php
@@ -155,7 +155,7 @@ class test_dalmp extends PHPUnit_Framework_TestCase {
     $uuids = array();
     for ($i = 0; $i < 10000; $i++) {
       $uuid = $this->db->uuid();
-      $this->assertTrue((boolean) preg_match('#[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}#', $uuid));
+      $this->assertTrue((boolean) preg_match('#[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[8,9,a,b][0-9a-f]{3}-[0-9a-f]{12}#', $uuid));
       $uuids[] = $uuid;
     }
     $uuids = array_unique($uuids);
