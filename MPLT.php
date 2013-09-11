1729be2b636e18323792e6566631a6758050fdc7
nbari <nbari@dalmp.com>
2013-09-11 11:06:40 +0100

    modified:   MPLT.php

--- MPLT.php
+++ MPLT.php
@@ -13,15 +13,28 @@ class MPLT {
   private $time_end;
   private $marks = array();
 
-  function __construct($decimals = 4) {
+  /**
+   * constructor
+   *
+   * @param int $decimals
+   */
+  public function __construct($decimals = 4) {
     $this->time_start = microtime(true);
     $this->decimals = $decimals;
   }
 
+  /**
+   * stop
+   */
   public function Stop() {
     $this->time_end = microtime(true);
   }
 
+  /**
+   * setMark
+   *
+   * @param string $name
+   */
   public function setMark($name = null) {
     $mark = microtime(true) - $this->time_start;
     $last_mark = end($this->marks)[0];
@@ -35,14 +48,28 @@ class MPLT {
     }
   }
 
+  /**
+   * getMark
+   *
+   * @param string $name
+   * @return array
+   */
   public function getMark($name = null) {
     return $name ? $this->marks[$name] : reset($this->marks);
   }
 
+  /**
+   * getMarks
+   *
+   * @return array
+   */
   public function getMarks() {
     return $this->marks;
   }
 
+  /**
+   * printMarks
+   */
   public function printMarks() {
     if ($this->marks) {
       $pad = $this->decimals * 2;
@@ -52,15 +79,20 @@ class MPLT {
         $pad = $max_length + 2;
       }
 
-      echo str_pad('mark', $pad), str_pad('time', $pad), 'elapsed-time', PHP_EOL;
+      echo str_pad('mark', $pad), str_pad('time', $pad), 'elapsed-time', $this->isCli(1);
       foreach ($this->marks as $mark => $values) {
-        echo str_pad($mark, $pad), str_pad($values[0], $pad), $values[1], PHP_EOL;
+        echo str_pad($mark, $pad), str_pad($values[0], $pad), $values[1], $this->isCli(1);
       }
     } else {
-      echo 'no defined marks',PHP_EOL;
+      echo 'no defined marks', $this->isCli(1);
     }
   }
 
+  /**
+   * getPageLoadTime
+   *
+   * @param bool $marks
+   */
   public function getPageLoadTime($marks = false) {
     if (empty($this->time_end)) {
       $this->Stop();
@@ -76,13 +108,34 @@ class MPLT {
     }
   }
 
+  /**
+   * getMemoryUsage
+   *
+   * @param bool $convert "Human-readable" output.
+   */
   public function getMemoryUsage($convert = false) {
     return $convert ? $this->convert(memory_get_usage(true)) : memory_get_usage(true);
   }
 
+  /**
+   * convert
+   *
+   * @param int $size
+   */
   public function convert($size) {
     $unit=array('B','KB','MB','GB','TB','PB');
     return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+  }
+
+  /**
+   * isCli()
+   *
+   * @param boolean $eol
+   * @return boolean or PHP_EOL, <br/>
+   */
+  public function isCli($eol = null) {
+    ($cli = (php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR']))) && $cli = $eol ? PHP_EOL : true;
+    return $cli ?: ($eol ? '<br/>' : false);
   }
 
 }
