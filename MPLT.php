49f30483a972ce05202df4f99d6984a74c98167a
nbari <nbari@dalmp.com>
2013-10-10 20:25:09 +0100

    modified:   MPLT.php

--- MPLT.php
+++ MPLT.php
@@ -133,7 +133,7 @@ class MPLT
      */
     public function convert($size)
     {
-        $unit=array('B','KB','MB','GB','TB','PB');
+        $unit = array('B','KB','MB','GB','TB','PB');
 
         return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
     }
