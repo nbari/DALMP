f950e5aad8266cb939d64e7d427e8ca7ff08b021
nbari <nbari@dalmp.com>
2014-03-20 20:38:23 +0000

    new file:   locale.php

---
+++ examples/database/locale.php
@@ -0,0 +1,36 @@
+<?php
+require_once '../../MPLT.php';
+$timer = new MPLT();
+require_once '../../src/dalmp.php';
+# ------------------------------------------------------------------------------
+
+$user = getenv('MYSQL_USER') ?: 'root';
+$password = getenv('MYSQL_PASS') ?: '';
+$host = getenv('MYSQL_HOST') ?: '127.0.0.1';
+$port = getenv('MYSQL_PORT') ?: '3306';
+
+$db = new DALMP\Database("utf8://$user:$password@$host:$port/dalmp");
+
+echo "Server time: ", $db->GetOne('SELECT NOW()');
+
+/**
+ *  load zone files to mysql
+ *  mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
+ */
+$db->PExecute('SET time_zone=?','+00:00');
+
+echo PHP_EOL, 'UTC time: ', $db->GetOne('SELECT NOW()');
+echo PHP_EOL, 'lc_time_names: ', $db->GetOne('SELECT @@lc_time_names');
+echo ': ', $db->PGetOne("SELECT DATE_FORMAT(?,'%W %a %M %b')", '2010-01-01');
+
+$db->PExecute('SET lc_time_names=?', 'es_MX');
+
+echo PHP_EOL, 'lc_time_names: ', $db->GetOne('SELECT @@lc_time_names');
+echo ': ', $db->PGetOne("SELECT DATE_FORMAT(?,'%W %a %M %b')", '2010-01-01');
+
+$db->PExecute('SET lc_time_names=?', 'pt_BR');
+echo PHP_EOL, 'lc_time_names: ', $db->GetOne('SELECT @@lc_time_names');
+echo ': ', $db->PGetOne("SELECT DATE_FORMAT(?,'%W %a %M %b')", '2010-01-01');
+
+# ------------------------------------------------------------------------------
+echo PHP_EOL,str_repeat('-', 80),PHP_EOL,'Time: ',$timer->getPageLoadTime(),' - Memory: ',$timer->getMemoryUsage(1),PHP_EOL,str_repeat('-', 80),PHP_EOL;
